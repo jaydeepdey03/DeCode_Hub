3eefb70706d0ce29939941ab398e48051f686832
ayuarya <ayu.arya72@gmail.com>
2023-03-21 09:53:47 +0530

    Added Models

---
+++ Backend/models/question.js
@@ -0,0 +1,24 @@
+const mongoose =require('mongoose')
+const Schema=mongoose.Schema;
+
+const questionSchema=new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    },
+    content:{
+        type:String,
+        required:true,
+    },
+    // upvotes:{
+    //     type:Number,
+    //     default:0,
+    // },
+    // downvotes:{
+    //     type:Number,
+    //     default:0
+    // }
+
+})
+module.exports=mongoose.model('Question',questionSchema)
