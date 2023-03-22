181837c3d20112a234f4af3f57ffc0312c4e87dd
Fidal <fidal15perfect@gmail.com>
2023-03-22 16:44:41 +0530

    fixed issues created by jaydeep

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,37 +6,37 @@ import { useEffect, useState } from "react"
 
 const Navbar = () => {
 
-    const [account, setAccount] = useState("")
+  const [account, setAccount] = useState("")
 
-    useEffect(() => {
-        (async () => {
-          const account = await getAccount();
-          setAccount(account);
-        })();
-      }, []);
+  useEffect(() => {
+    (async () => {
+      const account = await getAccount();
+      setAccount(account);
+    })();
+  }, []);
 
-      const onConnectWallet = async () => {
-        await connectWallet();
-        const account = await getAccount();
-        setAccount(account);
-      };
+  const onConnectWallet = async () => {
+    await connectWallet();
+    const account = await getAccount();
+    setAccount(account);
+  };
 
-    return (
-        <HStack padding={"7"} display={"flex"} justifyContent={"space-around"}>
-            <p>Logo</p>
-            <InputGroup width={"container.md"}>
-                <Input placeholder="Search Query" rounded={"3xl"} />
-                <InputRightElement children={<SearchIcon marginRight={"14"} />} />
-            </InputGroup>
-            <HStack>
-                {account ? 
-                <>
-                <Avatar height={"10"} width={"10"} />
-                <p>{account.slice(0, 10) + '...' + account.slice(-4)}</p></>
-                : <Button onClick={connectWallet}>Connect Wallet</Button>}
-            </HStack>
-        </HStack>
-    )
+  return (
+    <HStack padding={"7"} display={"flex"} justifyContent={"space-around"}>
+      <p>Logo</p>
+      <InputGroup width={"container.md"}>
+        <Input placeholder="Search Query" rounded={"3xl"} />
+        <InputRightElement children={<SearchIcon marginRight={"14"} />} />
+      </InputGroup>
+      <HStack>
+        {account ?
+          <>
+            <Avatar height={"10"} width={"10"} />
+            <p>{account.slice(0, 10) + '...' + account.slice(-4)}</p></>
+          : <Button onClick={onConnectWallet}>Connect Wallet</Button>}
+      </HStack>
+    </HStack>
+  )
 }
 
 export default Navbar
